e6ec04bf7f031c863195b0d63747541cb978aea2
SamuelLaguna <samanater11@gmail.com>
2024-03-18 20:50:43 -0700

    messed with the design more started adding id's

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+let cityName = document.getElementById("cityName");
+ let date = document.getElementById("date");
+let searchBar = document.getElementById("searchBar");
+let lo = document.getElementById("lo");
+let hi = document.getElementById("hi");
+let temp = document.getElementById("temp");
+let sprite1 = document.getElementById("sprite1");
+let sprite2 = document.getElementById("sprite2");
+let sprite3 = document.getElementById("sprite3");
+let sprite4 = document.getElementById("sprite4");
+let spirte5 = document.getElementById("sprite5");
+let sprite6 = document.getElementById("sprite6");
+

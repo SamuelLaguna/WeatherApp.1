7163e553b65968391e5720c46612abffd15c6d23
SamuelLaguna <samanater11@gmail.com>
2024-03-20 23:50:03 -0700

    linked up the api to the html added more id's

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,49 @@
+let cityData = [];
+let favArr = [];
+let defaultCity = "stockton";
+
+
+
+
 let cityName = document.getElementById("cityName");
  let date = document.getElementById("date");
 let searchBar = document.getElementById("searchBar");
 let lo = document.getElementById("lo");
 let hi = document.getElementById("hi");
 let temp = document.getElementById("temp");
-let sprite1 = document.getElementById("sprite1");
-let sprite2 = document.getElementById("sprite2");
-let sprite3 = document.getElementById("sprite3");
-let sprite4 = document.getElementById("sprite4");
-let spirte5 = document.getElementById("sprite5");
-let sprite6 = document.getElementById("sprite6");
+let icon1 = document.getElementById("icon1");
+let icon2 = document.getElementById("icon2");
+let icon3 = document.getElementById("icon3");
+let icon4 = document.getElementById("icon4");
+let icon5 = document.getElementById("icon5");
+let icon6 = document.getElementById("icon6");
+let favBtn = document.getElementById("favbtn");
+let cardTemp1 = document.getElementById("cardTemp1");
+let cardTemp2 = document.getElementById("cardTemp2");
+let cardTemp3 = document.getElementById("cardTemp3");
+let cardTemp4 = document.getElementById("cardTemp4");
+let cardTemp5 = document.getElementById("cardTemp5");
+
+
+
+
+async function getCity(cityOfChoice){
+    let apiResponse = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityOfChoice + "&units=imperial&appid=512e94f3b87d608e730d846d23470de1").then(Response => Response.json());
+    console.log(apiResponse);
+    cityName.innerText = apiResponse.name;
+    hi.innerText = apiResponse.temp_max;
+    temp.innerText = apiResponse.temp;
+    lo.innerText = apiResponse.temp_min;
+    cardTemp1.innerText = apiResponse.temp;
+    cardTemp2.innerText = apiResponse.temp;
+    cardTemp3.innerText = apiResponse.temp;
+    cardTemp4.innerHTML = apiResponse.temp;
+    cardTemp5.innerHTML = apiResponse.temp;
+    
+
+
+
+}
+getCity("Stockton");
+
 
